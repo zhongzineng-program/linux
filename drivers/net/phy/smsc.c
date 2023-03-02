c83b5efd9cf0be4062e26c0d07fcc7b78903df70
Zhong,Zineng <253947605@qq.com>
2023-03-03 00:23:10 +0800

    1.fix:增加脚本来把编译好的zImage和dtb放到tftpboot目录下:#0 2.fix:修改linux内核适配启用LAN8720的网卡驱动:#0

--- drivers/net/phy/smsc.c
+++ drivers/net/phy/smsc.c
@@ -59,6 +59,7 @@ static int smsc_phy_config_init(struct phy_device *phydev)
 
 static int smsc_phy_reset(struct phy_device *phydev)
 {
+	int timeout = 50000;
 	int rc = phy_read(phydev, MII_LAN83C185_SPECIAL_MODES);
 	if (rc < 0)
 		return rc;
@@ -67,21 +68,20 @@ static int smsc_phy_reset(struct phy_device *phydev)
 	 * in all capable mode before using it.
 	 */
 	if ((rc & MII_LAN83C185_MODE_MASK) == MII_LAN83C185_MODE_POWERDOWN) {
-		int timeout = 50000;
 
 		/* set "all capable" mode and reset the phy */
 		rc |= MII_LAN83C185_MODE_ALL;
 		phy_write(phydev, MII_LAN83C185_SPECIAL_MODES, rc);
-		phy_write(phydev, MII_BMCR, BMCR_RESET);
-
-		/* wait end of reset (max 500 ms) */
-		do {
-			udelay(10);
-			if (timeout-- == 0)
-				return -1;
-			rc = phy_read(phydev, MII_BMCR);
-		} while (rc & BMCR_RESET);
 	}
+		phy_write(phydev, MII_BMCR, BMCR_RESET);//复位phy芯片
+
+	/* wait end of reset (max 500 ms) */
+	do {
+		udelay(10);
+		if (timeout-- == 0)
+			return -1;
+		rc = phy_read(phydev, MII_BMCR);
+	} while (rc & BMCR_RESET);
 	return 0;
 }
 
---
+++ tftp_copy.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cp arch/arm/boot/zImage /home/zzn/linux/tftpboot/ -f
+cp arch/arm/boot/dts/imx6ull-alientek-emmc.dtb /home/zzn/linux/tftpboot/ -f
